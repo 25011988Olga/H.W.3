0a2e67f20ecdc4d4f828ff431842147dcd78a987
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-10 14:01:51 +0300

    ни как не могу решить

---
+++ семинар 3/Task3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+a = 0;
+t = 0;
+for(int i = 0; i<n/2; i++)
+t = args[i];
+a[i] = a[n-1-i];
+a[n-1-i] = t;
+if(n==t)
+Console.WriteLine("Является");
+if(a>t||a<t)
+Console.WriteLine("Не является");
